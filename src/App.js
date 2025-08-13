3c4818c06819c28268b49e51cf81445146ceca2f
ryanwhite1991023 <ryanwhite1991023@gmail.com>
2025-08-14 04:45:34 +1000

    2

--- src/App.js
+++ src/App.js
@@ -1,32 +1,22 @@
 import React from 'react';
 import Header from './components/Header';
 import Hero from './components/Hero';
-import ThemeToggle from './components/ThemeToggle';
 import About from './components/About';
 import Experience from './components/Experience';
 import Skills from './components/Skills';
 import Projects from './components/Projects';
-import Testimonials from './components/Testimonials';
-import GitHubActivity from './components/GitHubActivity';
-import ResumeDownload from './components/ResumeDownload';
-import Blog from './components/Blog';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <ThemeToggle />
       <Header />
       <Hero />
       <About />
       <Experience />
       <Skills />
       <Projects />
-      <Testimonials />
-      <GitHubActivity />
-      <ResumeDownload />
-      <Blog />
       <Contact />
       <Footer />
     </div>
